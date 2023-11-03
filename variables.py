dde5aa6743b604b66ff8e098a743820f85c3be20
Nagashree J <Nagashree.J@target.com>
2023-11-03 13:13:10 +0530

    variables and case sentivity

---
+++ variables.py
@@ -0,0 +1,26 @@
+def a = 1
+mutiple = 10/5
+x
+
+#error x
+'''
+Traceback (most recent call last):
+File "<stdin>", line 1, in <module>
+NameError: name 'x' is not defined
+'''
+
+
+case senstivity
+an_integer = 1
+#output 1
+AN_INTEGER
+An_Integer
+an_inteGer
+
+#error output
+   '''
+    Traceback (most recent call last):
+   File "<stdin>", line 1, in <module>
+   NameError: name 'AN_INTEGER' is not defined
+   '''
+
